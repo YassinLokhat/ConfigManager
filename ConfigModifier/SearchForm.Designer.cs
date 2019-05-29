0a9fb6480de858e32e036c09d4b14dcbe7bc84f4
Yassin Lokhat <yassinlokhat@gmail.com>
2019-05-29 08:16:43 +0400

    Adding Search Form

---
+++ ConfigModifier/SearchForm.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace ConfigModifier
+{
+    partial class SearchForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SearchForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbSearch = new System.Windows.Forms.TextBox();
+            this.rbSettingName = new System.Windows.Forms.RadioButton();
+            this.rbSettingValue = new System.Windows.Forms.RadioButton();
+            this.rbText = new System.Windows.Forms.RadioButton();
+            this.cbMatchCase = new System.Windows.Forms.CheckBox();
+            this.cbAllowRegex = new System.Windows.Forms.CheckBox();
+            this.lbResult = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Search :";
+            // 
+            // tbSearch
+            // 
+            this.tbSearch.Location = new System.Drawing.Point(65, 12);
+            this.tbSearch.Name = "tbSearch";
+            this.tbSearch.Size = new System.Drawing.Size(181, 20);
+            this.tbSearch.TabIndex = 0;
+            // 
+            // rbSettingName
+            // 
+            this.rbSettingName.AutoSize = true;
+            this.rbSettingName.Checked = true;
+            this.rbSettingName.Location = new System.Drawing.Point(252, 13);
+            this.rbSettingName.Name = "rbSettingName";
+            this.rbSettingName.Size = new System.Drawing.Size(87, 17);
+            this.rbSettingName.TabIndex = 10;
+            this.rbSettingName.TabStop = true;
+            this.rbSettingName.Text = "Setting name";
+            this.rbSettingName.UseVisualStyleBackColor = true;
+            // 
+            // rbSettingValue
+            // 
+            this.rbSettingValue.AutoSize = true;
+            this.rbSettingValue.Location = new System.Drawing.Point(345, 13);
+            this.rbSettingValue.Name = "rbSettingValue";
+            this.rbSettingValue.Size = new System.Drawing.Size(88, 17);
+            this.rbSettingValue.TabIndex = 20;
+            this.rbSettingValue.Text = "Setting Value";
+            this.rbSettingValue.UseVisualStyleBackColor = true;
+            // 
+            // rbText
+            // 
+            this.rbText.AutoSize = true;
+            this.rbText.Location = new System.Drawing.Point(439, 13);
+            this.rbText.Name = "rbText";
+            this.rbText.Size = new System.Drawing.Size(46, 17);
+            this.rbText.TabIndex = 31;
+            this.rbText.Text = "Text";
+            this.rbText.UseVisualStyleBackColor = true;
+            // 
+            // cbMatchCase
+            // 
+            this.cbMatchCase.AutoSize = true;
+            this.cbMatchCase.Checked = true;
+            this.cbMatchCase.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cbMatchCase.Location = new System.Drawing.Point(513, 15);
+            this.cbMatchCase.Name = "cbMatchCase";
+            this.cbMatchCase.Size = new System.Drawing.Size(83, 17);
+            this.cbMatchCase.TabIndex = 32;
+            this.cbMatchCase.Text = "Match Case";
+            this.cbMatchCase.UseVisualStyleBackColor = true;
+            // 
+            // cbAllowRegex
+            // 
+            this.cbAllowRegex.AutoSize = true;
+            this.cbAllowRegex.Location = new System.Drawing.Point(602, 15);
+            this.cbAllowRegex.Name = "cbAllowRegex";
+            this.cbAllowRegex.Size = new System.Drawing.Size(180, 17);
+            this.cbAllowRegex.TabIndex = 33;
+            this.cbAllowRegex.Text = "Allow Search Patern (*,+,?,!,^,$ )";
+            this.cbAllowRegex.UseVisualStyleBackColor = true;
+            // 
+            // lbResult
+            // 
+            this.lbResult.FormattingEnabled = true;
+            this.lbResult.Location = new System.Drawing.Point(12, 38);
+            this.lbResult.Name = "lbResult";
+            this.lbResult.Size = new System.Drawing.Size(770, 212);
+            this.lbResult.TabIndex = 34;
+            // 
+            // SearchForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(796, 261);
+            this.Controls.Add(this.lbResult);
+            this.Controls.Add(this.cbAllowRegex);
+            this.Controls.Add(this.cbMatchCase);
+            this.Controls.Add(this.rbText);
+            this.Controls.Add(this.rbSettingValue);
+            this.Controls.Add(this.rbSettingName);
+            this.Controls.Add(this.tbSearch);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(624, 300);
+            this.Name = "SearchForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Search a setting";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbSearch;
+        private System.Windows.Forms.RadioButton rbSettingName;
+        private System.Windows.Forms.RadioButton rbSettingValue;
+        private System.Windows.Forms.RadioButton rbText;
+        private System.Windows.Forms.CheckBox cbMatchCase;
+        private System.Windows.Forms.CheckBox cbAllowRegex;
+        private System.Windows.Forms.ListBox lbResult;
+    }
+}
---
+++ ConfigModifier/SearchForm.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace ConfigModifier
+{
+    public partial class SearchForm : Form
+    {
+        MainForm parent;
+
+        public SearchForm(MainForm parent)
+        {
+            InitializeComponent();
+            this.parent = parent;
+
+            tbSearch.KeyUp += TbSearch_KeyUp;
+            rbSettingName.CheckedChanged += RbSetting_CheckedChanged;
+            rbSettingValue.CheckedChanged += RbSetting_CheckedChanged;
+            rbText.CheckedChanged += RbSetting_CheckedChanged;
+            cbMatchCase.CheckedChanged += RbSetting_CheckedChanged;
+            cbAllowRegex.CheckedChanged += RbSetting_CheckedChanged;
+
+            lbResult.MouseDoubleClick += LbResult_MouseDoubleClick;
+
+            this.SizeChanged += SearchForm_SizeChanged;
+        }
+
+        private void LbResult_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (lbResult.SelectedIndices.Count == 0)
+                return;
+
+            string[] data = lbResult.SelectedItem.ToString().Split('\t');
+            string siteCode = (from x in parent.SiteFolderCode where x.Value == data[1].Replace("-" + data[0].ToUpper(), "") select x.Key).First();
+            string configPath = Environment.CurrentDirectory + "\\Settings\\SIT1-" + data[0] + @"\App_Config\Include\zzPG.GrowingFamilies.Version1\" + data[1] + @"\PG.GrowingFamilies.Version1." + siteCode + data[2];
+            Process.Start(@"C:\Users\ylokhat\Documents\Visual Studio 2015\Projects\XML_Explorer_Tools\XML_Explorer_Tools\bin\Release\XML_Explorer_Tools.exe", "\"" + configPath + "\"");
+        }
+
+        private void SearchForm_SizeChanged(object sender, EventArgs e)
+        {
+            this.lbResult.Size = new System.Drawing.Size(this.Size.Width - 40, this.Size.Height - 89);
+        }
+
+        private void RbSetting_CheckedChanged(object sender, EventArgs e)
+        {
+            search();
+        }
+
+        private void TbSearch_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                tbSearch.Text = "";
+                return;
+            }
+            else if (e.KeyCode != Keys.Enter)
+                return;
+
+            search();
+        }
+
+        private void search()
+        {
+            lbResult.Items.Clear();
+            if (tbSearch.Text == "")
+                return;
+
+            string[] configs = Directory.GetFiles("Settings", "*.config", SearchOption.AllDirectories);
+
+            foreach (string config in configs)
+            {
+                TextReader reader = new StreamReader(config);
+                string content = reader.ReadToEnd();
+                reader.Close();
+
+                if (!rbText.Checked)
+                {
+                    XmlDocument xmlDoc = new XmlDocument();
+                    xmlDoc.LoadXml(content);
+
+                    string xpath = "//setting[@" + (rbSettingName.Checked ? "name" : "value") + "]";
+                    var res = xmlDoc.SelectNodes(xpath);
+                    foreach (XmlNode node in res)
+                    {
+                        if (cbMatchCase.Checked)
+                        {
+                            if ((rbSettingName.Checked && !node.Attributes["name"].Value.Like(tbSearch.Text, cbAllowRegex.Checked, cbMatchCase.Checked)) || (rbSettingValue.Checked && !node.Attributes["value"].Value.Like(tbSearch.Text, cbAllowRegex.Checked, cbMatchCase.Checked)))
+                                continue;
+                        }
+                        else
+                        {
+                            if ((rbSettingName.Checked && !node.Attributes["name"].Value.ToLower().Like(tbSearch.Text.ToLower(), cbAllowRegex.Checked, cbMatchCase.Checked)) || (rbSettingValue.Checked && !node.Attributes["value"].Value.ToLower().Like(tbSearch.Text.ToLower(), cbAllowRegex.Checked, cbMatchCase.Checked)))
+                                continue;
+                        }
+
+                        string configName = Path.GetFileName(config.Replace("\\", "/"));
+                        configName = parent.GetLocalizedConfig(configName);
+                        if (configName == "")
+                            continue;
+
+                        string[] pathCompo = config.Split('\\');
+                        string environment = pathCompo[1].Split('-')[1];
+                        string site = pathCompo[5];
+
+                        lbResult.Items.Add(environment + "\t" + site + "\t" + configName + "\tSettingName=\"" + node.Attributes["name"].Value + "\"" + (node.Attributes.GetNamedItem("value") != null ? "\tSettingValue=\"" + node.Attributes["value"].Value + "\"" : ""));
+                    }
+                }
+                else
+                {
+                    if ((cbMatchCase.Checked && !content.Like(tbSearch.Text, cbAllowRegex.Checked, cbMatchCase.Checked)) || (!cbMatchCase.Checked && !content.ToLower().Like(tbSearch.Text.ToLower(), cbAllowRegex.Checked, cbMatchCase.Checked)))
+                        continue;
+
+                    string configName = Path.GetFileName(config.Replace("\\", "/"));
+                    configName = parent.GetLocalizedConfig(configName);
+                    if (configName == "")
+                        continue;
+
+                    string[] pathCompo = config.Split('\\');
+                    string environment = pathCompo[1];
+                    string site = pathCompo[5];
+                    lbResult.Items.Add(environment + "\t" + site + "\t" + configName);
+                }
+            }
+        }
+    }
+}
