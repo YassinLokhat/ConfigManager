832374fed07e47ebe990a3f351752aa0a29622b6
Yassin Lokhat <yassinlokhat@gmail.com>
2019-05-31 08:14:20 +0400

    Adding Extension Methods

---
+++ ConfigModifier/ExtentionMethods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConfigModifier
+{
+    public static class ExtentionMethods
+    {
+        public static bool Like(this string source, string toSearch, bool allowRegEx = false, bool matchCase = false)
+        {
+            if (!allowRegEx)
+                return matchCase ? source.Contains(toSearch) : source.ToLower().Contains(toSearch.ToLower());
+
+            toSearch = toSearch.Replace("*", @"([a]|[^a])*").Replace("?", @"([a]|[^a])?").Replace("+", @"([a]|[^a])+").Replace("!", @"([a]|[^a])");
+
+            Regex regex = new Regex(toSearch, matchCase ? RegexOptions.None : RegexOptions.IgnoreCase);
+
+            return regex.IsMatch(source);
+        }
+    }
+}
